eb33cb71123b75b33efa1ad438e310da953c18de
ikramuzzaman455173 <jakaria455173@gmail.com>
2023-12-06 01:38:35 +0600

    user controller create successfully

--- src/modules/user.controller.ts
+++ src/modules/user.controller.ts
@@ -1,3 +1,26 @@
 import { Request, Response } from 'express';
+import { userService } from './userService';
 
-const createUser = async (req: Request, res: Response) => {};
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { user: userData } = req.body;
+    const result = await userService.createUserIntoDB(userData);
+    res.status(200).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    // console.log(`Something went wrong: ${error}`);
+    res.status(404).json({
+      success: false,
+      message: 'User not found',
+      error: {
+        code: 404,
+        description: 'User not found!',
+      },
+    });
+  }
+};
+
+export const userController = { createUser };
